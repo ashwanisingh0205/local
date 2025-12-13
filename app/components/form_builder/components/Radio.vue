af8b5735e1af6e0f045157bdfe87d2264dc73824
Ayush Anand <ayushanand4814@gmail.com>
2025-12-13 12:27:14 +0530

    ayush

---
+++ app/components/form_builder/components/Radio.vue
@@ -0,0 +1,10 @@
+<template>
+    <UFormField :label="field.label">
+        <URadioGroup v-model="field.value" :items="field.items" :icon="field.icon" :label="field.label" />
+    </UFormField>
+</template>
+<script setup>
+const props = defineProps({
+    field: { type: Object, required: true },
+});
+</script>
